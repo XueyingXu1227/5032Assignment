369961c2f8417e197558881e5b38829bb6d8525c
Xueying Xu <xxuu0125@student.monash.edu>
2025-10-14 22:21:44 +1100

    Continue optimizing accessibility to ensure high scores in the accessibility checker.

--- src/components/FeatureCard.vue
+++ src/components/FeatureCard.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link :to="link" class="card h-100 text-decoration-none text-dark shadow-sm">
     <div class="card-body text-center">
-      <h5 class="card-title">{{ title }}</h5>
+      <h2 class="card-title">{{ title }}</h2>
       <p class="card-text">{{ description }}</p>
     </div>
   </router-link>
